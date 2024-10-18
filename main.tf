557849a8ccb5e469114a29681ef7d92ff0d98a67
Arunsai14 <arun.sai@sourcefuse.com>
2024-10-18 23:33:49 +0530

    warm

--- main.tf
+++ main.tf
@@ -108,15 +108,6 @@ resource "aws_opensearch_domain" "this" {
     custom_endpoint_certificate_arn = var.enable_custom_endpoint ? var.custom_certificate_arn : null
   }
 
-  # Auto-Tune settings
-  auto_tune {
-    desired_state = var.auto_tune_enabled ? "ENABLED" : "DISABLED"
-
-    maintenance_window {
-      start_time = "01:00"  # Set your desired maintenance start time in UTC
-      end_time   = "10:00"  # Set your desired maintenance end time in UTC
-    }
-  }
   # Enable logging
   log_publishing_options {
     log_type                 = var.log_type
