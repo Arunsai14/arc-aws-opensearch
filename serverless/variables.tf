06877f7f48c7fb204116890e8b5a93077f9aa9ce
Arunsai14 <arun.sai@sourcefuse.com>
2024-11-08 17:49:14 +0530

    add output.tf

--- serverless/variables.tf
+++ serverless/variables.tf
@@ -4,3 +4,15 @@ variable "vpc_id" {
   description = "VPC ID to allow network access."
   type        = string
 }
+
+variable "project_name" {
+  type        = string
+  default     = "sourcefuse"
+  description = "Project name"
+}
+
+variable "environment" {
+  type        = string
+  default     = "dev"
+  description = "ID element. Usually used for region e.g. 'uw2', 'us-west-2', OR role 'prod', 'staging', 'dev', 'UAT'"
+}
